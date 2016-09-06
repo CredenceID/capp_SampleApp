46d922993881d9629740cbaeb7e89bea97c80c80
Mark Evans <mevans7700@gmail.com>
2016-09-06 14:48:17 -0700

    implmented new close iris api

--- app/src/main/java/com/credenceid/sdkapp/IrisPage.java
+++ app/src/main/java/com/credenceid/sdkapp/IrisPage.java
@@ -1,7 +1,5 @@
 package com.credenceid.sdkapp;
 
-import java.io.File;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.SystemClock;
@@ -24,6 +22,8 @@ import com.credenceid.biometrics.Biometrics.OnIrisesCapturedListener;
 import com.credenceid.biometrics.Biometrics.ResultCode;
 import com.credenceid.biometrics.IrisQuality;
 
+import java.io.File;
+
 public class IrisPage extends LinearLayout implements PageView {
 	private static final String TAG = IrisPage.class.getName();
 
@@ -31,6 +31,7 @@ public class IrisPage extends LinearLayout implements PageView {
 	private SampleActivity mActivity;
 
 	private Button mCaptureBtn;
+	private Button mCloseBtn;
 	private ImageView mCaptureRight;
 	private ImageView mCaptureLeft;
 	private TextView mStatusTextView;
@@ -88,7 +89,15 @@ public class IrisPage extends LinearLayout implements PageView {
 		mCaptureBtn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				onCapture(v);
+				onCapture();
+			}
+		});
+		mCloseBtn = (Button) findViewById(R.id.close_btn);
+		mCloseBtn.setEnabled(false);
+		mCloseBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onClose();
 			}
 		});
 		mStatusTextView = (TextView) findViewById(R.id.status);
@@ -105,7 +114,7 @@ public class IrisPage extends LinearLayout implements PageView {
 			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 				mEyeSelection = eye_selection_types[position];
 				if (mCapturing)
-					onCapture(null);
+					onCapture();
 			}
 
 			@Override
@@ -130,26 +139,30 @@ public class IrisPage extends LinearLayout implements PageView {
 	public void doResume() {
 		setStatusText("");
 		setInfoText("");
-		setCapturing(false);
 	}
 
 	@Override
 	public void deactivate() {
 	}
-	
-	private void setCapturing(boolean is_capturing) {
-		mCaptureBtn.setActivated(is_capturing);
-		mCapturing = is_capturing;
+
+	private void enableCapture(boolean enable) {
+		// Enable capture by turning on capture button for user
+		mCaptureBtn.setEnabled(enable);
+		mCapturing = !enable;
 	}
 
-	private void onCapture(View v) {
+	private void resetCapture() {
 		mCaptureRight.setImageDrawable(null);
 		mCaptureLeft.setImageDrawable(null);
 		mPathnameLeft = null;
 		mPathnameRight = null;
 		setStatusText("");
 		setInfoText("");
-		setCapturing(true);
+	}
+
+	private void onCapture() {
+		resetCapture();
+		enableCapture(false);
 
 		mBiometrics.captureIrises(mEyeSelection, new OnIrisesCapturedListener() {
 
@@ -171,14 +184,36 @@ public class IrisPage extends LinearLayout implements PageView {
 				if (status != null)
 					setStatusText(status);
 				if (result == ResultCode.OK) {
-					setCapturing(false);
+					enableCapture(false);
 					convertToKind7(mPathnameLeft, mPathnameRight);
 
 				}
 
 			}
+
+			@Override
+			public void onCloseIrisScanner(Biometrics.CloseReasonCode closeReasonCode) {
+				// Log output for debugging
+				Log.d(TAG, "Iris Scanner closed:" + closeReasonCode.toString());
+				// Let uesr know why scanner reader closed
+				setStatusText("Iris Scanner closed:" + closeReasonCode.toString());
+				// Make close button unclickable, since the scanner has just closed
+				mCloseBtn.setEnabled(false);
+			}
 		});
 
+		mCloseBtn.setEnabled(true);
+	}
+
+	private void onClose() {
+		resetCapture();
+		enableCapture(true);
+
+		// Turn off close button since we are going to close everything
+		mCloseBtn.setEnabled(false);
+
+		// Close Iris Scanner
+		mBiometrics.closeIrisScanner();
 	}
 
 	private long uncompressed_size;
