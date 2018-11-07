87fed90fe04f35dadaedd64e7e8eeb6d050724b7
Avi Varshney <avarshney65@gmail.com>
2018-11-06 17:18:35 -0800

    Working on card reader.

--- app/src/main/java/com/cid/sdk/CardReaderActivity.java
+++ app/src/main/java/com/cid/sdk/CardReaderActivity.java
@@ -5,6 +5,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.cid.sdk.models.DeviceFamily;
 import com.cid.sdk.models.DeviceType;
@@ -18,65 +25,84 @@ import static com.credenceid.biometrics.Biometrics.ResultCode.FAIL;
 public class CardReaderActivity
 		extends Activity {
 
+	private static final String TAG = CardReaderActivity.class.getSimpleName();
+
 	// If write was good (SW1, SW2) = (0x90, 0x00).
 	// If you print out SW1 as an integer you will get "-112", this is due to "2's Complement".
 	// You can either check SW1 by comparing is against "-112" or you may compare it against the
 	// following two constants.
 	private final static byte SW1_SUCCESS = (byte) 0x90;
 	private final static byte SW2_SUCCESS = (byte) 0x00;
+
 	@SuppressLint("StaticFieldLeak")
 	private static BiometricsManager mBiometricsManager;
 	private static DeviceFamily mDeviceFamily;
 	private static DeviceType mDeviceType;
-	private final String TAG = CardReaderActivity.class.getSimpleName();
+
+	private boolean mIsCardReaderOpen = false;
+
+	// ------------------------------------ Layout Components ------------------------------------
+	private TextView mCardReaderStatusTextView;
+	private TextView mCardStatusTextView;
+	private TextView mDataTextView;
+
+	private CheckBox mSyncCheckbox;
+	private Button mOpenCloseButton;
+
+	private EditText mWriteDataEditText;
+	private Button mWriteToCardButton;
+	private Spinner mReadAPDUSelectSpinner;
+	private Button mReadFromCardButton;
+
 	// ------------------------------------ READ APDU ------------------------------------
 	// Reads 4096 (4K) number of bytes from card.
-	private final String mAPDURead4k = "FF"    // MiFare Card
+	private String mAPDURead4k = "FF"    // MiFare Card
 			+ "B0"                             // MiFare Card READ Command
 			+ "00"                             // P1
 			+ "00"                             // P2: Block Number
 			+ "001000";                        // Number of bytes to read
 	// Reads 2048 (2K) number of bytes from card.
-	private final String mAPDURead2k = "FF"    // MiFare Card
+	private String mAPDURead2k = "FF"    // MiFare Card
 			+ "B0"                             // MiFare Card READ Command
 			+ "00"                             // P1
 			+ "00"                             // P2: Block Number
 			+ "000800";                        // Number of bytes to read
 	// Reads 1024 (1K) number of bytes from card.
-	private final String mAPDURead1k = "FF"    // MiFare Card
+	private String mAPDURead1k = "FF"    // MiFare Card
 			+ "B0"                             // MiFare Card READ Command
 			+ "00"                             // P1
 			+ "00"                             // P2: Block Number
 			+ "000400";                        // Number of bytes to read
 	// ------------------------------------ WRITE APDU ------------------------------------
 	// Writes 4096 (4K) number of bytes to card.
-	private final String mAPDUWrite4k = "FF"   // MiFare Card
+	private String mAPDUWrite4k = "FF"   // MiFare Card
 			+ "D6"                             // MiFare Card READ Command
 			+ "00"                             // P1
 			+ "00"                             // P2: Block Number
 			+ "001000";                        // Number of bytes to read
 	// Writes 2048 (2K) number of bytes to card.
-	private final String mAPDUWrite2k = "FF"   // MiFare Card
+	private String mAPDUWrite2k = "FF"   // MiFare Card
 			+ "D6"                             // MiFare Card READ Command
 			+ "00"                             // P1
 			+ "00"                             // P2: Block Number
 			+ "000800";                        // Number of bytes to read
 	// Writes 1024 (1K) number of bytes to card.
-	private final String mAPDUWrite1k = "FF"   // MiFare Card
+	private String mAPDUWrite1k = "FF"   // MiFare Card
 			+ "D6"                             // MiFare Card READ Command
 			+ "00"                             // P1
 			+ "00"                             // P2: Block Number
 			+ "000400";                        // Number of bytes to read
 	// ------------------------------------ READ APDU ------------------------------------
 	// This APDU is used to read "mSpecialData" written to the card.
-	private final String mAPDUReadSpecialData = "FF"  // MiFare Card
+	private String mAPDUReadSpecialData = "FF"  // MiFare Card
 			+ "B0"                                    // MiFare Card READ Command
 			+ "00"                                    // P1
 			+ "01"                                    // P2: Block Number
 			+ "0E";                                   // Number of bytes to read
+
 	// -------------------------------- Special Data: Write to Card --------------------------------
 	// This is the data we will write to the card "CredenceID LLC"
-	private byte[] mSpecialData = new byte[] {
+	private byte[] mSpecialData = new byte[]{
 			// C	r	  e     d     e		n     c     e     I     D    ' '    L     L     C
 			0x43, 0x72, 0x65, 0x64, 0x65, 0x6e, 0x63, 0x65, 0x49, 0x44, 0x20, 0x4c, 0x4c, 0x43
 	};
@@ -88,11 +114,9 @@ public class CardReaderActivity
 																	int prevState,
 																	int currentState) -> {
 		// If currentState is 1, then no card is present.
-		if (currentState == 1) {
-			Log.d(TAG, "Card was removed, no card present.");
-			//noinspection UnnecessaryReturnStatement
-			return;
-		}
+		if (currentState == 1)
+			mCardStatusTextView.setText("Card is absent.");
+		else mCardStatusTextView.setText("Card is present.");
 
 		// currentStates [2, 6] represent a card present. If a card is present, code will reach
 		// here and attempt reads/writes with card.
@@ -123,6 +147,9 @@ public class CardReaderActivity
 		mDeviceType = MainActivity.getDeviceType();
 
 		mContext = this;
+
+		this.initializeLayoutComponents();
+		this.configureLayoutComponents();
 	}
 
 	@Override
@@ -139,17 +166,58 @@ public class CardReaderActivity
 	}
 
 	private void
+	initializeLayoutComponents() {
+		mCardReaderStatusTextView = findViewById(R.id.status_textview);
+		mCardStatusTextView = findViewById(R.id.cardstatus_textview);
+		mDataTextView = findViewById(R.id.data_textview);
+
+		mSyncCheckbox = findViewById(R.id.sync_checkbox);
+		mOpenCloseButton = findViewById(R.id.open_close_button);
+
+		mWriteDataEditText = findViewById(R.id.write_edittext);
+		mWriteToCardButton = findViewById(R.id.write_data_button);
+
+		mReadAPDUSelectSpinner = findViewById(R.id.read_apdu_selector_spinner);
+		mReadFromCardButton = findViewById(R.id.read_data_button);
+	}
+
+	private void
+	configureLayoutComponents() {
+		this.setReadWriteComponentEnable(false);
+
+		mWriteDataEditText.clearFocus();
+
+		mOpenCloseButton.setOnClickListener((View v) -> {
+			mOpenCloseButton.setEnabled(false);
+
+			mCardStatusTextView.setText("");
+
+			if (mIsCardReaderOpen)
+				mBiometricsManager.cardCloseCommand();
+			else this.openCardReader();
+		});
+	}
+
+	private void
 	openCardReader() {
-		final String localTag = TAG + ":openCardReader";
-		Log.d(TAG, "openCardReader()");
+		mCardReaderStatusTextView.setText("Opening card reader, please wait...");
 
 		mBiometricsManager.cardOpenCommand(new Biometrics.CardReaderStatusListener() {
 			@Override
 			public void onCardReaderOpen(Biometrics.ResultCode resultCode) {
-				if (resultCode == FAIL) {
-					Log.e(localTag, "FAILED to open CardReader.");
+				mOpenCloseButton.setEnabled(true);
+
+				if (FAIL == resultCode) {
+					mCardReaderStatusTextView.setText("Failed to open card reader.");
 					return;
 				}
+
+				mIsCardReaderOpen = true;
+
+				mCardReaderStatusTextView.setText("Card reader opened.");
+				mOpenCloseButton.setText("Close CardReader");
+
+				setReadWriteComponentEnable(true);
 
 				// If card reader opened successfully, register a listener will be invoked each time
 				// card reader's status changes. Meaning that anytime a card is placed/removed
@@ -160,9 +228,32 @@ public class CardReaderActivity
 			@Override
 			public void onCardReaderClosed(Biometrics.ResultCode resultCode,
 										   Biometrics.CloseReasonCode closeReasonCode) {
-				Log.d(localTag, "CardReader closed: " + closeReasonCode.name());
+				mOpenCloseButton.setEnabled(true);
+
+				if (FAIL == resultCode)
+					return;
+
+				mIsCardReaderOpen = false;
+
+				mCardReaderStatusTextView.setText("Card reader closed: " + closeReasonCode.name());
+				mOpenCloseButton.setText("Open CardReader");
+
+				setReadWriteComponentEnable(false);
 			}
 		});
+	}
+
+	private void
+	setReadWriteComponentEnable(boolean enabled) {
+		mWriteDataEditText.setEnabled(enabled);
+		mWriteToCardButton.setEnabled(enabled);
+		mReadAPDUSelectSpinner.setEnabled(enabled);
+		mReadFromCardButton.setEnabled(enabled);
+	}
+
+	private void displayKeyboard() {
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.toggleSoftInputFromWindow(mWriteDataEditText.getApplicationWindowToken(), InputMethodManager.SHOW_FORCED, 0);
 	}
 
 	/* This method attempts to read 1K, 2K, and 4K blocks of data off of a card. You pass in either
