b184be5ec582a937d61827638bf27475b37d0999
shweta <shwetasa@cybage.com>
2020-12-24 18:49:35 +0530

    Added intent for innovetrics.

--- app/src/main/java/com/credenceid/sdkapp/CameraActivity.kt
+++ app/src/main/java/com/credenceid/sdkapp/CameraActivity.kt
@@ -7,8 +7,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import com.credenceid.sdkapp.android.camera.CallCredenceCameraApp
 import com.credenceid.sdkapp.util.CredenceCameraAppLivenessResult
-import com.credenceid.sdkapp.util.credenceCamera_Constant.FEATURE_FACE_LIVENESS
-import com.credenceid.sdkapp.util.credenceCamera_Constant.PROVIDER_NEUROTECH
+import com.credenceid.sdkapp.util.credenceCamera_Constant.*
 import kotlinx.android.synthetic.main.act_camera.*
 
 /**
@@ -60,9 +59,25 @@ class CameraActivity : ComponentActivity() {
 
         liveness_innovatrics_btn?.isEnabled = true
         liveness_innovatrics_btn?.setOnClickListener {
-            val launchIntent = packageManager.getLaunchIntentForPackage("com.credenceid.capp_credencecamera")
-            launchIntent.flags = 0
-            launchIntent?.let { startActivityForResult(it, 100) }
+            val startCredenceCameraApp = registerForActivityResult(CallCredenceCameraApp(
+                    FEATURE_FACE_LIVENESS,
+                    PROVIDER_INNOVATRICS)){ result: CredenceCameraAppLivenessResult? ->
+                // Handle the return
+                if (result != null) {
+                    if(result.sdkResult == 1) {
+                        status_textview.text = "Result = " + result.sdkResult + "\nLiveness Score = " + result.livenessScore
+
+                    }else {
+                        status_textview.text = "ERROR = " + result.sdkResult
+                    }
+                } else {
+                    Log.e(TAG, "Fail to get Live Subject template.");
+                }
+            }
+            startCredenceCameraApp.launch(0)
+//            val launchIntent = packageManager.getLaunchIntentForPackage("com.credenceid.capp_credencecamera")
+//            launchIntent.flags = 0
+//            launchIntent?.let { startActivityForResult(it, 100) }
         }
 
         barcode_cid_btn?.isEnabled = true
