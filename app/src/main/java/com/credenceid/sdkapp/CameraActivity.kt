5a470946ac700c54b61c5cbf1992a11c990a167b
Michael <michael.ferrato@credenceid.com>
2021-02-23 15:31:21 +0100

    Replaced camera sample with Liveness API test

--- app/src/main/java/com/credenceid/sdkapp/CameraActivity.kt
+++ app/src/main/java/com/credenceid/sdkapp/CameraActivity.kt
@@ -2,18 +2,17 @@
 
 package com.credenceid.sdkapp
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import android.view.View
 import androidx.activity.ComponentActivity
-import com.credenceid.sdkapp.android.camera.CallCredenceCameraApp
-import com.credenceid.sdkapp.util.BitmapUtils
-import com.credenceid.sdkapp.util.CredenceCameraAppLivenessResult
-import com.credenceid.sdkapp.util.credenceCamera_Constant.*
+import com.credenceid.biometrics.Biometrics
+import com.credenceid.face.FaceEngine
+import com.developer.filepicker.controller.DialogSelectionListener
+import com.developer.filepicker.model.DialogConfigs
+import com.developer.filepicker.model.DialogProperties
+import com.developer.filepicker.view.FilePickerDialog
 import kotlinx.android.synthetic.main.act_camera.*
 import java.io.File
 
@@ -23,6 +22,9 @@ import java.io.File
  */
 private val TAG = "CID"
 
+private var dialog: FilePickerDialog? = null
+private var videoUri : Uri? = null
+private var sStartTime = System.currentTimeMillis()
 
 class CameraActivity : ComponentActivity() {
 
@@ -31,6 +33,36 @@ class CameraActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_camera)
         this.configureLayoutComponents()
+
+
+
+        btn_browse?.isEnabled = true
+        btn_browse.setOnClickListener(View.OnClickListener {
+            v: View? -> dialog?.show()
+        })
+
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        val properties = DialogProperties()
+        properties.selection_mode = DialogConfigs.SINGLE_MODE
+        properties.selection_type = DialogConfigs.FILE_SELECT
+        properties.root = File(DialogConfigs.DEFAULT_DIR)
+        properties.error_dir = File(DialogConfigs.DEFAULT_DIR)
+        properties.offset = File(DialogConfigs.DEFAULT_DIR)
+
+        dialog = FilePickerDialog(this, properties)
+
+        dialog!!.properties = properties
+        dialog!!.setTitle("Select Video")
+        dialog!!.setDialogSelectionListener(DialogSelectionListener { files: Array<String?> ->
+            if (files.size > 0) {
+                val file =  File(files[0])
+                status_textview.text = file.absolutePath
+                videoUri = Uri.parse(file.absolutePath)
+            }
+        })
     }
 
     override fun onDestroy() {
@@ -39,113 +71,48 @@ class CameraActivity : ComponentActivity() {
 
     }
 
+
+
     /**
      * Configure all objects in layout file, set up listeners, views, etc.
      */
     private fun configureLayoutComponents() {
 
-        back_liveness_neurotech_btn?.isEnabled = true
-        back_liveness_neurotech_btn?.setOnClickListener {
-            startLiveness(PROVIDER_NEUROTECH, CAMERA_BACK);
-        }
+        liveness_neurotech_btn?.isEnabled = true
+        liveness_neurotech_btn?.setOnClickListener {
 
-        front_liveness_neurotech_btn?.isEnabled = true
-        front_liveness_neurotech_btn?.setOnClickListener {
-            startLiveness(PROVIDER_NEUROTECH, CAMERA_FRONT);
-        }
+            Log.d(TAG, "SampleApp => Starting liveness verification" )
 
-        back_liveness_innovatrics_btn?.isEnabled = true
-        back_liveness_innovatrics_btn?.setOnClickListener {
-            startLiveness(PROVIDER_INNOVATRICS, CAMERA_BACK);
-        }
+            sStartTime = System.currentTimeMillis()
 
-        front_liveness_innovatrics_btn?.isEnabled = true
-        front_liveness_innovatrics_btn?.setOnClickListener {
-            startLiveness(PROVIDER_INNOVATRICS, CAMERA_FRONT);
-        }
+            App.BioManager!!.verifyFaceLiveness(videoUri, object : FaceEngine.OnVerifyFaceLiveness {
+                override fun onVerifyFaceLiveness(resultCode: Biometrics.ResultCode?, status: String?, sdkResult: String? , score: Int, progress: Int, blinkResult: Boolean) {
+                    var displayedResult = "Verifcation completed: "+ "\n"
+                    when {
+                        Biometrics.ResultCode.OK == resultCode -> {
+                            displayedResult =  displayedResult + "Status = " + status+ "\n"
+                            displayedResult =  displayedResult + "SDK result = " + sdkResult + "\n"
+                            displayedResult =  displayedResult + "Liveness score = " + score + "\n"
+                            displayedResult =  displayedResult + "Subject Blink detection = " + blinkResult + "\n"
+                            displayedResult =  displayedResult + "Performing time  = " + (System.currentTimeMillis() - sStartTime)
 
-        back_liveness_nfv_btn?.isEnabled = true
-        back_liveness_nfv_btn?.setOnClickListener {
-            startLiveness(PROVIDER_NFV, CAMERA_BACK);
-        }
-
-        front_liveness_nfv_btn?.isEnabled = true
-        front_liveness_nfv_btn?.setOnClickListener {
-
-            startLiveness(PROVIDER_NFV, CAMERA_FRONT);
-        }
-
-        barcode_cid_btn?.isEnabled = true
-        barcode_cid_btn?.setOnClickListener {
-            val startCredenceCameraApp = registerForActivityResult(CallCredenceCameraApp(
-                    FEATURE_BARCODE,
-                    PROVIDER_CID,
-                    LIVENESS_MODE_PASSIVE,
-                    CAMERA_BACK)){ result: CredenceCameraAppLivenessResult? ->
-                // Handle the return
-                if (result != null) {
-                    if(result.sdkResult == 1) {
-                        status_textview.text = "Result = OK " +
-                                "\nMessage = " + result.sdkResultMessage
-                        displayImage(result.faceImageUri)
-                    }else {
-                        status_textview.text = "Result = FAILED"
+                        }
+                        Biometrics.ResultCode.INTERMEDIATE == resultCode -> {
+                            displayedResult = "Performing verification: "+ "\n" + status + " (" + progress + "%)"
+                        }
+                        Biometrics.ResultCode.FAIL == resultCode -> {
+                            displayedResult =  displayedResult + "Status = " + status+ "\n"
+                            displayedResult =  displayedResult + "SDK result = " + sdkResult + "\n"
+                            displayedResult =  displayedResult + "Liveness score = " + score + "\n"
+                            displayedResult =  displayedResult + "Subject Blink detection = " + blinkResult + "\n"
+                        }
                     }
-                } else {
-                    Log.e(TAG, "Fail to get Live Subject template.");
+                    status_textview.text = displayedResult
                 }
-            }
-            startCredenceCameraApp.launch(0)
+            })
         }
 
-        mrz_btn?.isEnabled = true
-        mrz_btn?.setOnClickListener {
 
-            Toast.makeText(applicationContext, "Not implemented", Toast.LENGTH_SHORT)
-        }
-
-    }
-
-    private fun startLiveness(provider: Int, camera: Int){
-
-        val startCredenceCameraApp = registerForActivityResult(CallCredenceCameraApp(
-                FEATURE_FACE_LIVENESS,
-                provider,
-                LIVENESS_MODE_PASSIVE,
-                camera)){ result: CredenceCameraAppLivenessResult? ->
-            // Handle the return
-            if (result != null) {
-                if(result.sdkResult == 1) {
-                    status_textview.text = "Result = OK " +
-                            "\nLiveness Score = " + result.livenessScore +
-                            "\nMessage = " + result.sdkResultMessage
-
-                    displayImage(result.faceImageUri)
-
-                }else {
-                    status_textview.text = "Result = FAILED" +
-                            "\nLiveness Score = " + result.livenessScore +
-                            "\nMessage = " + result.sdkResultMessage
-                }
-            } else {
-                Log.e(TAG, "Fail to get Live Subject template.");
-            }
-        }
-        startCredenceCameraApp.launch(0)
-    }
-
-    private fun displayImage( imageUri:Uri){
-        val faceImageFile = File(imageUri.path)
-        if(faceImageFile.exists()){
-            Log.d(TAG, "Image file Exist!!!")
-            var image = BitmapFactory.decodeFile(faceImageFile.path)
-            if(image.height > 475) {
-                Log.d(TAG, "Image file loaded: image size = " + image.byteCount)
-                image = Bitmap.createScaledBitmap(image, (475*image.width)/image.height, 475, false)
-            }
-            faceResultImageView.setImageBitmap(image)
-            Log.d(TAG, "Image displayed")
-        }
     }
 
 
