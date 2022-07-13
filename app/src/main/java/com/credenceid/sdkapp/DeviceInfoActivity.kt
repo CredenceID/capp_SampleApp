eb43aa4077ac122bab88000d4afdb12149884dbe
michael.ferrato <michael.ferrato@gmail.com>
2022-07-13 12:16:16 +0200

    [MINOR] Updating to androidx dependencies

--- app/src/main/java/com/credenceid/sdkapp/DeviceInfoActivity.kt
+++ app/src/main/java/com/credenceid/sdkapp/DeviceInfoActivity.kt
@@ -5,9 +5,9 @@ import android.content.Intent
 import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.os.Build
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.credenceid.biometrics.Biometrics.ResultCode
 import com.credenceid.biometrics.Biometrics.ResultCode.*
 import com.credenceid.biometrics.BiometricsManager
